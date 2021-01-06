920752fb951a3a5cc6bbec9595482175a5cd42df
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2021-01-06 10:34:54 +0100

    Getting value in converterform and calculate with return: basic

--- converterform.php
+++ converterform.php
@@ -3,4 +3,16 @@
    ini_set('display_startup_errors', '1');
    error_reporting(E_ALL);
 
+   // TODO Figure out what a form submit actually means / does
+   // TODO How to get the form data after the submit?
+   // TODO Calculate the amount in the desired valuta
+   // TODO Figure out a way to provide this data in the HTML
+   $currencyRate = 0.014;
+
+   if (isset($_POST['peso'])) {
+      $peso = $_POST['peso'];
+   }
+   $euro = $peso*$currencyRate;
+   
+   echo "<p> $peso DOP = $euro EUR </p>";
 ?>
